e453807e597d4807d2ee45819adf51a3c6bb7f6f
João Araújo <joaofilipearaujo@gmail.com>
2013-10-18 00:50:01 +0100

    Missing localization strings for framework default admin panel options and update to the README.md file.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ UniPress Alpha (not usable yet)
 
 WordPress Theme Framework used on my future themes, feel free to use as you see fit :)
 
-NOTE: This is not a theme, just a framework (plugin) used by a parent theme.
+NOTE: This is not a theme, just a framework (plugin) used by a parent theme and I'll include a sample theme in the future.
--- admin/options-unipress.php
+++ admin/options-unipress.php
@@ -14,52 +14,52 @@ function optionsframework_get_unipress_options() {
 	if( current_theme_supports( 'unipress-fonts' ) ) {
 
 		// FONTS MANAGER -------------------------------------------------------------------
-		$options[] = array( "name" => "",
-							"type" => "heading",
-							"location" => 'unipress-fonts-manager' );
+		$options[] = array( 'name' => '',
+							'type' => 'heading',
+							'location' => 'unipress-fonts-manager' );
 
-			$options[] = array( "name" => "Update available Google web fonts",
-								"desc" => "The number of available fonts in the Google web font directory is constantly being updated, hit the \"Update\" button to grab the latest list of fonts. You can preview the available fonts in the <a href=\"http://www.google.com/webfonts\">Google web fonts directory</a>.",
-								"id" => "fonts_update_google",
-								"type" => "fonts_update_google",
-								"std" => "",
-								"location" => 'unipress-fonts-manager' );
+			$options[] = array( 'name' => __( 'Update available Google web fonts', 'unipress' ),
+								'desc' => __( 'The number of available fonts in the Google web font directory is constantly being updated, hit the "Update" button to grab the latest list of fonts. You can preview the available fonts in the <a href="http://www.google.com/webfonts">Google web fonts directory</a>.', 'unipress' ),
+								'id' => 'fonts_update_google',
+								'type' => 'fonts_update_google',
+								'std' => '',
+								'location' => 'unipress-fonts-manager' );
 
-			$options[] = array( "name" => "Include the following font subsets:",
-								"desc" => "NOTE: not all fonts support all subsets, for more information please check the <a href=\"http://www.google.com/webfonts\">Google web fonts directory</a>.",
-								"id" => "fonts_subsets_google",
-								"std" => "",
-								"type" => "multicheck",
-								"options" => array( 
-									'cyrillic' => 'Cyrillic',
-									'cyrillic-ext' => 'Cyrillic Extended',
-									'greek' => 'Greek',
-									'greek-ext' => 'Greek Extended',
-									'latin' => 'Latin',
-									'latin-ext' => 'Latin Extended',
-									'vietnamese' => 'Vietnamese' ),
-								"location" => 'unipress-fonts-manager' );
+			$options[] = array( 'name' => __( 'Include the following font subsets:', 'unipress' ),
+								'desc' => __( 'NOTE: not all fonts support all subsets, for more information please check the <a href="http://www.google.com/webfonts">Google web fonts directory</a>.', 'unipress' ),
+								'id' => 'fonts_subsets_google',
+								'std' => '',
+								'type' => 'multicheck',
+								'options' => array( 
+									'cyrillic' => __( 'Cyrillic', 'unipress' ),
+									'cyrillic-ext' => __( 'Cyrillic Extended', 'unipress' ),
+									'greek' => __( 'Greek', 'unipress' ),
+									'greek-ext' => __( 'Greek Extended', 'unipress' ),
+									'latin' => __( 'Latin', 'unipress' ),
+									'latin-ext' => __( 'Latin Extended', 'unipress' ),
+									'vietnamese' => __( 'Vietnamese', 'unipress' ) ),
+								'location' => 'unipress-fonts-manager' );
 	}
 
 	// SIDEBAR MANAGER -------------------------------------------------------------------
-	$options[] = array( "name" => "",
-						"type" => "heading",
-						"location" => 'unipress-sidebars-manager' );
+	$options[] = array( 'name' => '',
+						'type' => 'heading',
+						'location' => 'unipress-sidebars-manager' );
 							
-		$options[] = array( "name" => "Create new custom sidebar:",
-							"desc" => "",
-							"id" => "sidebar_create",
-							"type" => "sidebar_create",
-							"class" => "mini",
-							"std" => "",
-							"location" => 'unipress-sidebars-manager' );
+		$options[] = array( 'name' => __( 'Create new custom sidebar:', 'unipress' ),
+							'desc' => '',
+							'id' => 'sidebar_create',
+							'type' => 'sidebar_create',
+							'class' => 'mini',
+							'std' => '',
+							'location' => 'unipress-sidebars-manager' );
 
-		$options[] = array( "name" => "Available custom sidebars:",
-							"desc" => "",
-							"id" => "sidebar_list",
-							"type" => "sidebar_list",
-							"std" => "",
-							"location" => 'unipress-sidebars-manager' );
+		$options[] = array( 'name' => __( 'Available custom sidebars:', 'unipress' ),
+							'desc' => '',
+							'id' => 'sidebar_list',
+							'type' => 'sidebar_list',
+							'std' => '',
+							'location' => 'unipress-sidebars-manager' );
 
 	return $options;
 }
